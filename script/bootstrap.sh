b0ff33531707e5e12b66068080b344f2a143516e
@slovacus <slovacus@gmail.com>
2016-11-07 14:17:23 -0500

    [#10] Fix: settings bootstrap

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 # -*- coding: utf-8 -*-
 
-export PROJECT_NAME=slide-git-for-devs
+export PROJECT_NAME=slide-git-flow-developer
 # Vars Dir
 export ROOT_DIR="`pwd`"
 export RESOURCES_DIR="$ROOT_DIR/resources"
 export SOURCE_DIR="$ROOT_DIR"
 export STATIC_DIR="$SOURCE_DIR/static/"
+export ETC_DIR="$SOURCE_DIR/etc"
 
 export VIRTUALENV=$PROJECT_NAME
